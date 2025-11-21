c471b9b113d8fc6a41300180c439dbe4a729d397
sknadim1993 <smnadim.gov@gmail.com>
2025-11-21 20:09:50 +0530

    Update app.ts

--- server/app.ts
+++ server/app.ts
@@ -85,12 +85,8 @@ export default async function runApp(
   // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = parseInt(process.env.PORT || '5000', 10);
-  server.listen({
-    port,
-    host: "0.0.0.0",
-    reusePort: true,
-  }, () => {
-    log(`serving on port ${port}`);
-  });
+  const PORT = process.env.PORT || 10000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
 }
